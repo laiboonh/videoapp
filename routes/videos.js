e6eb407680094ff718c3baadeb14d948476b7241
Boon Hui <boonhui@blockfi.com>
2021-03-05 19:18:42 +0800

    Working timestamp show and nested directory content listing

--- routes/videos.js
+++ routes/videos.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-const videosFolder = './public/videos';
+const videos_folder = './public/videos';
 
-var video_files;
-fs.readdir(videosFolder, (err, files) => { video_files = files; });
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else {
+            let mtime = stat.mtime;
+            /* Is a file */
+            results.push({name: file.slice(videos_folder.length), time: mtime});
+        }
+    });
+    return results;
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('videos', { title: 'Video App', video_files: video_files });
+  res.render('videos', { title: 'Video App', video_files: walk(videos_folder) });
 });
 
 module.exports = router;
